e9f58577470603c862c64ec98e5322a483a9e3c0
roxas <jeropastrana@gmail.com>
2022-10-21 17:02:30 +0200

    aaa

--- src/com/cieep/Main.java
+++ src/com/cieep/Main.java
@@ -1,5 +1,87 @@
+package com.cieep;
+
+import com.cieep.modelos.Alumno;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+
+
+        int opcion;
+        do {
+            opcion = menu(scanner);
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    Alumno alumno = crearAlumno(scanner);
+                    listaAlumnos.add(alumno);
+                    break;
+                case 2:
+                    try {
+                        cargarAlumnos(listaAlumnos);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 3:
+                    guardarAlumnos(listaAlumnos);
+                    break;
+                case 4:
+                    muestraAlumnos(listaAlumnos);
+                    break;
+
+            }
+
+        } while (opcion != 4);
+
     }
+
+    private static void cargarAlumnos(ArrayList<Alumno> listaAlumnos) throws IOException {
+        listaAlumnos.clear();
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar", "librerias/HijoLector.jar");
+        pb.redirectErrorStream(true);
+
+        Process proceso = pb.start();
+        OutputStream os = proceso.getOutputStream(); // ESTO ES PARA ESCRIBIR
+        PrintStream ps = new PrintStream(os);
+
+    }
+
+    private static Alumno crearAlumno(Scanner scanner) {
+        System.out.println("Dime el nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Dime los apellidos: ");
+        String apellidos = scanner.nextLine();
+        System.out.println("Dime el DNI: ");
+        String dni = scanner.nextLine();
+
+        return new Alumno(nombre, apellidos, dni);
+    }
+
+    private static void guardarAlumnos(ArrayList<Alumno> listaAlumnos) {
+
+    }
+
+    private static void muestraAlumnos(ArrayList<Alumno> listaAlumnos) {
+        for (Alumno a : listaAlumnos) {
+            System.out.println(a.toString());
+        }
+    }
+
+    private static int menu(Scanner scanner) {
+        System.out.println("1: Crear alumno");
+        System.out.println("2: Cargar alumnos");
+        System.out.println("3: Guardar alumnos");
+        System.out.println("4: Salir");
+        return scanner.nextInt();
+    }
+
 }
--- src/com/cieep/modelos/Alumno.java
+++ src/com/cieep/modelos/Alumno.java
@@ -1,2 +1,59 @@
-package com.cieep.modelos;public class Alumno {
+package com.cieep.modelos;
+
+public class Alumno {
+    private String nombre, apellidos, dni;
+
+    @Override
+    public String toFile() {
+        return nombre + ";" + apellidos + ";" + dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", dni='" + dni + '\'' +
+                '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+
+    public Alumno(String alumnoCodificado) {
+        this.nombre = alumnoCodificado.split(";") [0];
+        this.apellidos = alumnoCodificado.split(";") [1];
+        this.dni = alumnoCodificado.split(";") [2];
+    }
+
 }
